a6dcf4e3f70090db4bdcec41264c205a45c8f5f1
Jake Mulhern <mulherje@gmail.com>
2024-02-09 16:46:58 -0700

    Added iteration exercise

---
+++ iteration/iteration_test.go
@@ -0,0 +1,29 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+// To run $ go test
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+// To run $ go test -bench=.
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
+}
